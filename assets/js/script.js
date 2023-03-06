22f2e07f8e71d1a3316f026c94ee88f7fbcb5f10
saifaliaxioned <saifalis@axioned.com>
2023-03-06 11:43:03 +0530

    js added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,19 @@
-/* Author: 
+const rangeInput = document.querySelector('.zoom-range');
+const image = document.querySelector('.image-box img');
+const figure = document.querySelector('.image-figure');
 
-*/
+if (rangeInput.value == 50) {
+  figure.classList.add('image-size');
+} else {
+  figure.classList.remove('image-size');
+}
 
-
-
-
-
+rangeInput.addEventListener('input', () => {
+  const rangeValue = rangeInput.value / 20;
+  if (rangeValue > 0.5) {
+    image.style.transform = `scale(${rangeValue})`;
+  };
+});
 
 
 

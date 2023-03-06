1788dcf3ecc74d39bc7a140e7ae2eea68fd9488c
saifaliaxioned <saifalis@axioned.com>
2023-03-06 19:21:32 +0530

    changes updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,6 @@ const rangeInput = document.querySelector('.zoom-range');
 const image = document.querySelector('.image-box img');
 const figure = document.querySelector('.image-figure');
 
-if (rangeInput.value == 50) {
-  figure.classList.add('image-size');
-} else {
-  figure.classList.remove('image-size');
-}
-
 rangeInput.addEventListener('input', () => {
   const rangeValue = rangeInput.value / 20;
   if (rangeValue > 0.5) {
